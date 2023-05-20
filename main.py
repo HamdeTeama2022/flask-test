3424e353a14432add8be6a68a2d896d7199d0289
HamdeTeama2022 <117295119+HamdeTeama2022@users.noreply.github.com>
2023-05-20 05:26:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ def video_feed():
 
 if __name__ == '__main__':
     
-    app.run(host="0.0.0.0",port=80,debug=True)
+    app.run(host="0.0.0.0",port=5000,debug=True)
 
 
 # Release the video capture and close all windows when the app is stopped
